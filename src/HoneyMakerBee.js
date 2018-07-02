5aca247df5861354f27f19078ffb2ca32840b292
Michael Lee <lee.cs.michael@gmail.com>
2018-07-01 18:53:45 -0700

    complete using ES6

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+
+  constructor() {
+  	
+  	super();
+  	this.age = 10;
+  	this.job = 'make honey';
+  	this.honeyPot = 0;
+  }
+
+  makeHoney() {
+  	this.honeyPot++;
+
+  	return this.honeyPot;
+  }
+
+  giveHoney() {
+  	if(this.honeyPot) {
+  		this.honeyPot--;
+  	}
+  	return this.honeyPot;
+  }
 };
